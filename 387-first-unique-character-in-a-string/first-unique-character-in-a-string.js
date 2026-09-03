2c2a5e6c712171805de7831b5c6aa67d55135cef
Pratik_49 <pratiksanap789@gmail.com>
2026-09-03 22:41:23 +0530

    Time: 39 ms (29.29%) | Memory: 59 MB (79.57%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const map = new Map();
+
+    for(const ch of s){
+        map.set(ch,(map.get(ch) || 0) + 1);
+    }
+
+    for(let i = 0 ; i < s.length ; i++){
+        if(map.get(s[i]) === 1) return i;
+    }
+    return -1;
+};
